7da84036632af51ead91e7cc694460c58aef863f
stacksideflow <ivvkl@hotmail.com>
2018-11-22 01:26:03 +0300

    Add single-sexp debugger shortcut

--- clj-args.el
+++ clj-args.el
@@ -1,34 +1,74 @@
-(defun strip-functions (str)
-  (replace-regexp-in-string "([^.]*)\\|(.*)" "arg" str))
+  (setq-default vector-pattern "\\[[^.]*?\\]" )
+  (setq-default map-destructuring-pattern "{:keys \\[[^.]*?}")
+  (setq-default fn-pattern "(.*)*")
 
-(defun strip-destructuring (str)
-  (replace-regexp-in-string "{:keys \\[\\|\\]}" "" str))
+  (defun extract-destructured-args (str)
+    (setq-local defunced (replace-regexp-in-string fn-pattern ":arg-side-args-emacs" str))
+    (setq-local saved-match (string-match vector-pattern defunced))
+    (setq-local saved-match-str (match-string 0 defunced))
+    (if (null saved-match-str)
+      (substring saved-match-str 1 -1)
+      ""))
 
-(defun remove-odd (list)
-  (loop for i in list for idx from 0 unless (oddp idx) collect i))
+  (defun strip-forms (str)
+    (replace-regexp-in-string vector-pattern ":arg-side-args-emacs"
+     (replace-regexp-in-string map-destructuring-pattern ":arg-side-args-emacs"
+      (replace-regexp-in-string fn-pattern ":arg-side-args-emacs" str))))
 
-(defun arg->arg-and-label (arg)
-  (concat "\n\"___" arg " \"" " " arg))
+  (defun remove-odd (list)
+    (loop for i in list
+          for idx from 0
+          unless (oddp idx)
+          collect i))
 
-(defun str-vector->labeled-args (str-vector sexp-type)
-  (setq-local after-functions (strip-functions str-vector))
-  (setq-local after-destr (strip-destructuring after-functions))
-  (setq-local vector-trimmed (split-string (substring after-destr 1 -1)))
-  (let ((arg-list (if (string= sexp-type "def") vector-trimmed
-                    (remove-odd vector-trimmed))))
-    (concat "(println \"***args are \" "
-            (mapconcat 'arg->arg-and-label arg-list " ") ")\n")))
+;;todo place destrctured args into their correct positions
+  (defun arg->arg-and-label (arg)
+    (when (not (string= ":arg-side-args-emacs" arg))
+        (concat "\n\"___" arg " \"" " " arg)))
 
-(defun arg-side-debug
-    (&optional
-     n)
+  (defun str-vector->labeled-args (str-vector sexp-type)
+(message sexp-type)
+    (setq-local trimmed-vector (substring str-vector 1 -1))
+    (setq-local flattened-arguments (split-string (strip-forms trimmed-vector)))
+    (setq-local flattened-destructured-arguments (split-string (extract-destructured-args trimmed-vector)))
+    (let ((arg-list (if (string= sexp-type "def")
+                     flattened-arguments
+                     (remove-odd flattened-arguments))))
+    (concat "(println \"***args are \" " (mapconcat 'arg->arg-and-label
+                                                    (append
+                                                     arg-list
+                                                     flattened-destructured-arguments) " ") ")\n")))
+
+  (defun arg-side-debug (&optional n)
+    (interactive "P")
+    (setq-local insert-at (point))
+    (sp-beginning-of-sexp)
+    (setq-local sexp-type (evil-yank-characters (point) (+ 3 (point))))
+    (down-list)
+    (sp-backward-up-sexp)
+    (let* ((args (evil-cp-yank-sexp 1)))
+      (goto-char insert-at)
+      (insert-string (str-vector->labeled-args
+                      args sexp-type))))
+
+(defun debug-spit-symbol (&optional n)
   (interactive "P")
-  (setq-local insert-at (point))
-  (sp-beginning-of-sexp)
-  (setq-local sexp-type (evil-yank-characters (point)
-                                              (+ 3 (point))))
-  (down-list)
-  (sp-backward-up-sexp)
-  (let* ((args (evil-cp-yank-sexp 1)))
-    (goto-char insert-at)
-    (insert-string (str-vector->labeled-args args sexp-type))))
+  (let* ((yanked (evil-paste-before 0)))
+  (insert-string (concat "\n(println \">>>>spitting data "
+                         yanked ":\" "
+                         yanked ")\n"))))
+
+  (dolist (m '(clojure-mode))
+    (spacemacs/set-leader-keys-for-major-mode m
+      "j" 'cider-project-reset
+      "J" 'cider-dev
+      "sj" 'cider-connect-sibling-cljs
+      "sa" 'cider-default-connect
+      "sC" 'cider-replicate-connection
+      "hc" 'clojure-cheatsheet))
+
+  (dolist (m '(clojure-mode clojurescript-mode))
+    (spacemacs/set-leader-keys-for-major-mode m
+      "da" 'arg-side-debug
+      "ds" 'debug-spit-symbol
+      "gk" 'cider-find-keyword))
